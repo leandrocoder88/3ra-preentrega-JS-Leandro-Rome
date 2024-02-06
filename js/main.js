72f9403329478e44371c110f24c28307894f0c85
Leandro Romegialli <leandrocodex88@gmail.com>
2024-02-05 23:05:51 -0300

    avance 1 de tercera entrega JS

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const selectMenu = document.querySelector("select#Menu")
+const selectUbicacion = document.querySelector("select#ubicacion")
+const inputPersonas = document.querySelector("input#personas")
+const valorVelada = document.querySelector("span#valorVelada")
+
+const btnPresupuesto = document.querySelector("button.button.button-outline")
+
+const btnGuardar = document.querySelector("span.guardar")
+const arrayHistorial = []
+
+function cargarMenues() {
+    datosMenu.forEach((menu)=> {
+        selectMenu.innerHTML += `<option>${menu.tipo}</option>`
+    })
+}
+
+function cargarUbicaciones() {
+    datosUbicacion.forEach((ubicacion)=> {
+        selectUbicacion.innerHTML += `<option>${ubicacion.tipo}</option>`
+    })
+}
+
+cargarMenues()
+cargarUbicaciones()
+
+function retornarFactorMenu(tipoMenu) {
+    return datosMenu.find((menu)=> menu.tipo === tipoMenu)
+}
+
+function retornarFactorUbicacion(tipoUbica) {
+    return datosUbicacion.find((ubicacion)=> ubicacion.tipo === tipoUbica)
+}
+
+function cotizarVelada() {
+    if (selectMenu.value !== 'Seleccionar...' && selectUbicacion.value !== 'Seleccionar...' && (inputPersonas.value >= 1 || inputPersonas.value <= 20)) {
+        let personas = inputPersonas.value
+        let factorMenu = retornarFactorMenu(selectMenu.value)
+        let factorUbicacion = retornarFactorUbicacion(selectUbicacion.value)
+
+        const generartotal = new generarTotal(personas, factorMenu, factorUbicacion, costoCubiertos)
+        return generartotal.total()
+    }
+}
+
+btnPresupuesto.addEventListener("click", ()=> {
+    let resultado = cotizarVelada()
+
+    if (resultado !== 'Error') {
+        valorVelada.textContent = resultado}})
+
+        btnGuardar.addEventListener("click", ()=> {
+            const historialGenerarTotal = {
+                fecha: Date(),
+                menu: selectMenu.value,
+                ubicacion: selectUbicacion.value,
+                personas: inputPersonas.value,
+                Velada: valorVelada.textContent
+            }
+        
+            localStorage.setItem("HistorialGenerarTotal", JSON.stringify(historialGenerarTotal))
+        })        
---
+++ js/variables.js
@@ -0,0 +1,16 @@
+const datosMenu = [{tipo: 'Comida Vegana + consumicion', factor: 1.30},
+                        {tipo: 'Pastas + consumicion', factor: 1.05},
+                        {tipo: 'Carnes + consumicion', factor: 1.25},
+                        {tipo: 'Picadas + consumicion', factor: 1.20}]
+
+
+
+
+const datosUbicacion = [{tipo: 'Mesa con vista al mar', factor: 1.29},
+                        {tipo: 'Mesa en terraza', factor: 1.22},
+                        {tipo: 'Mesa frente al escenario', factor: 1.15},
+                        {tipo: 'Mesa en sector vip', factor: 1.50},
+                        {tipo: 'Barra', factor: 1.00}]
+
+const costoCubiertos = 14999.99
+
